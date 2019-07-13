ed5c56a8ea992cfa8fe49ca2a39571e0d70a944f
Ariunbold <ariun1118@gmail.com>
2019-07-13 10:59:09 -0400

    MVP DONE

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
 // import actions
+import {getStarwars} from '../actions'
 
 class CharacterListView extends React.Component {
   constructor() {
@@ -10,12 +11,15 @@ class CharacterListView extends React.Component {
   }
 
   componentDidMount() {
-    // call our action
+     this.props.getStarwars();
   }
 
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      return (
+        <h1>Not ready yet.</h1>
+      )
     }
     return (
       <div className="CharactersList_wrapper">
@@ -25,11 +29,19 @@ class CharacterListView extends React.Component {
   }
 }
 
+const mapStateToProps = state => {
+  return{
+    characters : state.charsReducer.characters,
+    isFetching: state.charsReducer.isFetching
+  }
+}
+
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps, /* mapStateToProps replaces null here */
   {
+    getStarwars
     /* action creators go here */
   }
 )(CharacterListView);
